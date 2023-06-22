a6ee5b2f008a7b8168c9bc20c0b525c044b849c7
parkyoungwoon <75366063+parkyoungwoon@users.noreply.github.com>
2023-06-22 20:14:17 +0900

    Add files via upload

--- src/main/java/com/studySample/studyDescription/post/PostController.java
+++ src/main/java/com/studySample/studyDescription/post/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class PostController {
@@ -30,6 +32,33 @@ public class PostController {
     @PostMapping("/post/save.do")
     public String openPostSave(final PostRequest params) {
         postService.savePost(params);
+        return "redirect:/post/list.do";
+    }
+
+    //게시글 조회 페이지
+    @GetMapping("/post/list.do")
+    public String openPostList(Model model){
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts",posts);
+        return "post/list";
+    }
+
+    //게시물 상세 페이지
+    @GetMapping("/post/view.do")
+    public String openPostView(@RequestParam final Long id, Model model) {
+        //주소가 "/post/view.do?id=1" 이런식으로 들어오는 방식을 쿼드 스트링이라고함
+        if(id != null){
+            //이부분을 stream 이랑 제내릭으로 바꿀 수 있는지 확인 필요함
+            PostResponse post = postService.findPostById(id);
+            model.addAttribute("post",post);
+        }
+        return "post/view";
+    }
+
+    // 게시글 저장 페이지
+    @PostMapping("/post/update.do")
+    public String openPostUpdate(final PostRequest params) {
+        postService.updatePost(params);
         return "redirect:/post/list.do";
     }
 
